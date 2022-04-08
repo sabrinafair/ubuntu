5a0a7f45a28945755951a74db172584c9f27b6e1
sabrinafair <Sabrina Ferras>
2022-04-08 14:43:10 -0700

    I think looks done

--- cst334/week6/alphabet_step2.c
+++ cst334/week6/alphabet_step2.c
@@ -1,7 +1,7 @@
 //Name: Sabrina Ferras
-//Date: 4-7-2022
+//Date: 4-8-2022
 //Title: Lab 6 - Producer Consumer program to produce and consume the alphabet
-//Description: This program makes threads to produce letters A - B and threads to consume (print) the letters 
+//Description: This program makes threads (a producer and consumer thread) to produce letters (printing out which letter was produces) A - B and to consume the letters and print them to output
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
@@ -16,11 +16,12 @@ sem_t mutex;
 int buffer[N];	//to hold the letters
 int fill = 0;	//used the format from the book for the put and get
 int use = 0;
-
-pthread_t p[N];
-pthread_t c[N];
-int asciiPlace = 64;	//one less than the beginning of the uppercase alphabet
-
+int itemsConsumed = 26;
+#define NTHREADS 1
+pthread_t p[NTHREADS];
+pthread_t c[NTHREADS];
+int asciiPlace = 65;	//one less than the beginning of the uppercase alphabet
+int loops = 26;
 void put(int value){
 	buffer[fill] = value;
 	fill = (fill + 1) % N;
@@ -33,28 +34,33 @@ int get(){
 }
 
 void *producer(void *arg){
-//	do{
-	asciiPlace++;
+	do{
+	if(asciiPlace > 90) break;	//to exit out of the loop
+	int i = asciiPlace++;
 	sem_wait(&empty);
 	sem_wait(&mutex);
-	put(asciiPlace);
+	put(i);
+	printf("Producer thread producing letter: %c\n", i);
 	sem_post(&mutex);
 	sem_post(&full);
-//	}while(1);
+//	}
+	}while(1);
+	
 }
 
 void *consumer(void *arg){
-//	do{
 	char tmp;	//I added
+	do{
+	if(loops <= 0) break;
 	sem_wait(&full);
 	sem_wait(&mutex);
 	tmp = get();
 	sem_post(&mutex);
 	sem_post(&empty);
-	printf("%c\n", tmp);
-//	}while(1);
+	printf("Consumer thread consuming: %c\n", tmp);
+	loops--;
+	}while(1);
 }
-
 
 int main(int argc, char *argv[]){
 
@@ -62,12 +68,12 @@ int main(int argc, char *argv[]){
 	sem_init(&full, 0, 0);
 	sem_init(&mutex, 0, 1);	//set to one for lock
 	int i;
-	for(i = 0; i < N; i++){	//creating the producer and consumer threads
+	for(i = 0; i < NTHREADS; i++){	//creating the producer and consumer threads
 		pthread_create(&p[i], NULL, producer, (void *)NULL);
 		pthread_create(&c[i], NULL, consumer, (void *)NULL);
 	}
 
-	for(i = 0; i < N; i++){	//waiting for the producer and consumer threads
+	for(i = 0; i < NTHREADS; i++){	//waiting for the producer and consumer threads
 		pthread_join(p[i], NULL);
 		pthread_join(c[i], NULL);
 	}
